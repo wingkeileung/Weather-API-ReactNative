78cfff8a296041664ce5098e23ce980608a0d19f
Wing Kei Leung <WinkyLeung@hotmail.com>
2018-01-13 11:31:30 -0800

    laying down basic style

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <View style={styles.header}>
+          <Text>
+            <TextInput style={styles.TextInput} placeholder='Enter City' />
+          </Text>
+        </View>
+        <View style={styles.content}>
+          <View>
+            <Text style={styles.temperature}>
+              12
+            </Text>
+          </View>  
+          <View>
+            <Text style={styles.city}>
+              City Name
+            </Text>
+          </View>  
+          <View>
+            <Text style={styles.country}>
+              Country Name
+            </Text>
+          </View>  
+        </View>
+        <View style={styles.footer}>
+          <Text>
+            footer
+          </Text>
+        </View>
       </View>
     );
   }
@@ -18,4 +44,46 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'red',
+    width: '100%',
+  },
+  content: {
+    flex: 3,
+    backgroundColor: 'green',
+    width: '100%',
+  },
+  footer: {
+    flex: 2,
+    backgroundColor: 'blue',
+    width: '100%',
+  },
+  TextInput: {
+    width: 200,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    backgroundColor: 'gray',
+  },
+  temperature: {
+    fontSize: 50,
+    color: 'black',
+    paddingTop: 50,
+    paddingLeft: 30,
+  },
+  city: {
+    paddingTop: 30,
+    paddingBottom: 50,
+    color: 'black',
+    fontSize: 25,
+    paddingLeft: 30,
+  },
+  country: {
+    paddingTop: 10,
+    paddingLeft: 30,
+    fontSize: 20,
+  }
 });
